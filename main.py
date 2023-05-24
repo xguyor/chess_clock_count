a284e911e0c0a3c8adbce9597baa82413b56b603
Guy Or <94971164+xguyor@users.noreply.github.com>
2023-05-24 23:09:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+
 import time
 import re
 import os
-
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pygame
+import webbrowser
+
+
 def main():
 
-    # Set up Chrome driver
+    #
+    # # Specify the path to your HTML file
+    # html_file = "file:///Users/guyor/PycharmProjects/chess_clock_alert/login.html"
+    # # Open the HTML file in the default web browser
+    # webbrowser.open_new_tab(html_file)
+
+
+
     chrome_options = Options()
-    user_data_directory = os.getcwd()
-    chrome_options.add_argument(f"user-data-dir={user_data_directory}\\userdata")
+    # user_data_directory = os.getcwd()
+    # chrome_options.add_argument(f"user-data-dir={user_data_directory}\\userdata")
     service = Service('/usr/local/bin')  # Replace with the actual path to the chromedriver executable
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service)
 
     # Open chess.com and log in
-    driver.get("https://www.chess.com")
+    driver.get("https://www.chess.com/login_and_go?returnUrl=https://www.chess.com/")
     # Perform login steps here (enter username and password, click login)
+
+    # Check if the login credentials file exists
+    credentials_file_path = "/Users/guyor/PycharmProjects/chess_clock_alert/credentials.txt"
+
+
+    try:
+        with open(credentials_file_path, "r") as file:
+            # Read the credentials from the file
+            username, password = file.read().strip().split(",")
+            # Find the username/email input field and enter the value
+            username_input = driver.find_element(By.ID, "username")
+            username_input.send_keys(username)
+
+            # Find the password input field and enter the value
+            password_input = driver.find_element(By.ID, "password")
+            password_input.send_keys(password)
+
+            # Find the login button and click it
+            login_button = driver.find_element(By.ID, "login")
+            login_button.click()
+
+    except FileNotFoundError:
+        # If the file doesn't exist, prompt the user for the credentials
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        # Save the credentials to the file for future runs
+        with open(credentials_file_path, "w") as file:
+            file.write(f"{username},{password}")
 
 
 
@@ -27,13 +67,14 @@ def main():
     game_url = "https://www.chess.com/play/online/new"
     driver.get(game_url)
 
-
     # Find the button element
     button = driver.find_element(By.CSS_SELECTOR, "button[data-cy='new-game-time-selector-button']")
 
     # Click the button
     button.click()
-    time.sleep(5)
+
+
+    # time.sleep(5)
 
     time_option = input("Enter the option number (1min - 60,3min - 180, or 5min - 300): ")
     # Find the button element
@@ -49,7 +90,7 @@ def main():
     # Click the button
     button.click()
 
-    # # Wait for the game to load
+    # Wait for the game to load
     # time.sleep(7)
 
 
@@ -61,6 +102,7 @@ def main():
 
     # Start monitoring the clock
     while True:
+
         # clock_element = driver.execute_script("return document.querySelector('span[data-cy=\"clock-time\"]').innerText;")
         element = driver.find_element(By.CSS_SELECTOR, '.clock-component.clock-bottom .clock-time-monospace')
         current_time = element.text
@@ -81,6 +123,9 @@ def main():
             pygame.mixer.music.play()
             while True:
                 while len(driver.find_elements(By.CSS_SELECTOR, '.clock-component.clock-bottom.clock-player-turn')) != 0:
+                    if len(driver.find_elements(By.CSS_SELECTOR, '.board-modal-container-container')) != 0:
+                        pygame.mixer.music.stop()
+                        break
                     if not pygame.mixer.music.get_busy():
                         pygame.mixer.music.unpause()
                     else:
@@ -88,7 +133,11 @@ def main():
 
                 pygame.mixer.music.pause()
                 while len(driver.find_elements(By.CSS_SELECTOR,'.clock-component.clock-bottom.clock-player-turn')) == 0:
+                    if len(driver.find_elements(By.CSS_SELECTOR, '.board-modal-container-container')) != 0:
+                        pygame.mixer.music.stop()
+                        break
                     continue
+
         # Check if the clock has run out
         if current_time == "0:00":
             pygame.mixer.music.stop()
@@ -99,7 +148,7 @@ def main():
 
     # Close the browser
     pygame.quit()
-    driver.quit()
+    # driver.quit()
 
 if __name__ == '__main__':
     main()
